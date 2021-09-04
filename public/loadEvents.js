569a2c0a0c4e2c25b509d2d1399cba207642321a
jpbonch <jp.bonchristiano@gmail.com>
2021-09-04 02:49:28 +0100

    Script to dynamically generate chat buttons

---
+++ public/loadEvents.js
@@ -0,0 +1,20 @@
+
+function loadNbaEvents(){
+    fetch("/events/nba")
+    .then(response => response.json())
+    .then(jsonResult => {console.log(jsonResult); return jsonResult})
+    .then(jsonResult => display(jsonResult))
+
+    function display(jsonResult){
+        for (let event of jsonResult.api.games){
+            var box = document.createElement("div");
+            box.addEventListener("click", () => {window.location = "/chat/nba/" + event.gameId}, false)
+            box.innerText = event.startTimeUTC;
+            box.innerText += " " + event.vTeam.fullName;
+            box.innerText += " vs" + event.hTeam.fullName;
+            box.className = "box";
+            document.getElementById("container").appendChild(box);
+        }
+    }
+}
+document.getElementById("nbaButton").addEventListener("click", loadNbaEvents, false)
